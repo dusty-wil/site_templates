5508d6b22e5c5de7d2d4ab9e591b766b72827525
Dusty Williams <dusty.wil@gmail.com>
2018-01-03 20:39:25 -0800

    various style and layout changes

--- mobile_first_flex_aware/js/lib.js
+++ mobile_first_flex_aware/js/lib.js
@@ -20,31 +20,34 @@ function gid(el_id)
 function hasClass(el, find_class)
 {
     var reg = new RegExp(find_class, 'g');
-    
     return reg.test(el.className);
 }
 
 
 /**
- * 
- *
- *
+ * adds a class to an element 
+ * @param el the element to add the class to
+ * @param add_class the class to add
+ * @return boolean
  */
 function addClass(el, add_class)
 {
-    if (hasClass(el, add_class)) { return true; } 
+    if (hasClass(el, add_class)) { return false; } 
     el.className += " " + add_class;
+    return true;
 }
 
 
 /**
- * 
- *
- *
+ * removes a class from an element
+ * @param el the element to remove the class from
+ * @param rem_class the class to remove
+ * @return boolean 
  */
 function remClass(el, rem_class)
 {
-    if (!hasClass(el, rem_class)) { return true; }
+    if (!hasClass(el, rem_class)) { return false; }
     var reg = new RegExp(" " + rem_class, 'g');
     el.className = el.className.replace(reg, "");
+    return true;
 }
